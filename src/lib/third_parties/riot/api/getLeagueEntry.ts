a2ea67a83382633c17b8bdd8977a358235f14585
Wooseop Kim <wooseop@protonmail.com>
2024-05-23 05:35:00 +0900

    URI encode path parameter

--- src/lib/third_parties/riot/api/getLeagueEntry.ts
+++ src/lib/third_parties/riot/api/getLeagueEntry.ts
@@ -6,7 +6,7 @@ interface Params {
 
 export default async function getLeagueEntry({ summonerId }: Params) {
   const res = await fetch(
-    `https://kr.api.riotgames.com/tft/league/v1/entries/by-summoner/${summonerId}`,
+    `https://kr.api.riotgames.com/tft/league/v1/entries/by-summoner/${encodeURIComponent(summonerId)}`,
     {
       headers: {
         'X-Riot-Token': RIOT_API_KEY,
