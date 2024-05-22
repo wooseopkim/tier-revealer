921a91334145e81ca5e6315f7dd91ea78f37ec33
Wooseop Kim <wooseop@protonmail.com>
2024-05-23 06:34:51 +0900

    Unify units when comparing `exp` and now

--- src/lib/third_parties/cloudflare/kv/getRiotTokens.ts
+++ src/lib/third_parties/cloudflare/kv/getRiotTokens.ts
@@ -13,7 +13,7 @@ export default async function getRiotTokens({ namespace, idToken }: Params) {
   const jwk = jwks.find((jwk) => jwk.kid === kid)!;
   const key = await importJWK(jwk, jwk.alg)
   const { payload: claims } = await jwtVerify(idToken, key);
-  if (new Date().valueOf() >= new Date(claims.exp!).valueOf()) {
+  if (new Date().valueOf() >= new Date(claims.exp! * 1000).valueOf()) {
     return null;
   }
   return await namespace.get(claims.sub!);
