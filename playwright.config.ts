01a814991a4cb3c2d1a12f00910f8dae781067c8
Wooseop Kim <wooseop@protonmail.com>
2024-05-25 01:05:38 +0900

    Fix broken Playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,15 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 const config: PlaywrightTestConfig = {
   webServer: {
     command: 'npm run build && npm run preview',
-    port: 4173,
+    ignoreHTTPSErrors: true,
+    url: 'https://localhost:4173',
+  },
+  use: {
+    browserName: 'chromium',
+    launchOptions: {
+      args: ['--ignore-certificate-errors'],
+    },
+    baseURL: 'https://localhost:4173',
   },
   testDir: 'tests',
   testMatch: /(.+\.)?(test|spec)\.[jt]s/,
