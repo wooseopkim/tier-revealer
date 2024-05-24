43f34734595b939a3df3570cce9eff22804b7607
Wooseop Kim <wooseop@protonmail.com>
2024-05-24 23:57:21 +0900

    Handle async `Promise` executor error

--- src/lib/components/RiotSignOn.svelte
+++ src/lib/components/RiotSignOn.svelte
@@ -3,9 +3,9 @@
     PUBLIC_RIOT_SIGN_ON_CLIENT_ID,
     PUBLIC_RIOT_SIGN_ON_REDIRECT_URI,
   } from '$env/static/public';
-  import { onMount } from 'svelte';
-  import riotIdToken from '$lib/store/riotIdToken';
   import cachedFetch from '$lib/cache/cachedFetch';
+  import riotIdToken from '$lib/store/riotIdToken';
+  import { onMount } from 'svelte';
 
   const signOnUrl = new URL('https://auth.riotgames.com');
   signOnUrl.pathname = 'authorize';
@@ -34,6 +34,7 @@
         return;
       }
 
+      // eslint-disable-next-line no-async-promise-executor
       identity = new Promise(async (resolve, reject) => {
         const url = '/api/riot/me';
         const fetch = cachedFetch({ url, ttl: 60 });
@@ -41,7 +42,12 @@
           headers: {
             Authorization: `Bearer ${value}`,
           },
-        });
+        }).catch(reject);
+
+        if (typeof res !== 'object') {
+          reject();
+          return;
+        }
 
         if (!res.ok) {
           reject(res.statusText);
