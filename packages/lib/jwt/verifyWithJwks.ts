cf51d01999f944fbf0546f96b5da383af2ed4568
Wooseop Kim <wooseop@protonmail.com>
2024-05-26 02:49:41 +0900

    Provide fallback `alg`

--- packages/lib/jwt/verifyWithJwks.ts
+++ packages/lib/jwt/verifyWithJwks.ts
@@ -1,8 +1,8 @@
 import { decodeProtectedHeader, importJWK, jwtVerify, type JWK } from 'jose';
 
 export default async function verifyWithJwks(token: string, jwks: JWK[]) {
-  const { kid } = decodeProtectedHeader(token);
+  const { alg, kid } = decodeProtectedHeader(token);
   const jwk = jwks.find((jwk) => jwk.kid === kid)!;
-  const key = await importJWK(jwk, jwk.alg);
+  const key = await importJWK(jwk, jwk.alg ?? alg);
   return await jwtVerify(token, key);
 }
