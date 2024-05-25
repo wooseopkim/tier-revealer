84c945f87beed8b8dd766725eaafaae6f202ca1c
Wooseop Kim <wooseop@protonmail.com>
2024-05-25 19:11:59 +0900

    Configure Jest as test runner for `lib`

--- packages/lib/cache/cachedFetch.test.ts
+++ packages/lib/cache/cachedFetch.test.ts
@@ -1,4 +1,3 @@
-import { beforeEach, describe, expect, it, vi, type MockedFunction } from 'vitest';
 import cachedFetch from './cachedFetch';
 
 describe(cachedFetch, () => {
@@ -9,18 +8,18 @@ describe(cachedFetch, () => {
   let cache: Partial<Cache>;
 
   beforeEach(() => {
-    vi.stubGlobal('fetch', async () => new Response());
+    jest.spyOn(globalThis, 'fetch').mockImplementation(async () => new Response());
 
     cache = {
-      match: vi.fn(async () => undefined),
-      put: vi.fn(async () => {}),
+      match: jest.fn(async () => undefined),
+      put: jest.fn(async () => {}),
     };
     const caches: Partial<CacheStorage> = {
       async open() {
         return cache as Cache;
       },
     };
-    vi.stubGlobal('caches', caches);
+    globalThis['caches'] = caches as CacheStorage;
   });
 
   it.each(params)('caches responses', async (...args) => {
@@ -28,7 +27,7 @@ describe(cachedFetch, () => {
 
     await fetch('https://example.com');
 
-    const req = (cache.put as MockedFunction<(typeof Cache)['prototype']['put']>).mock
+    const req = (cache.put as jest.MockedFunction<(typeof Cache)['prototype']['put']>).mock
       .calls[0][0] as Request;
     expect(req.url).toBe('https://example.com/');
   });
@@ -36,7 +35,9 @@ describe(cachedFetch, () => {
   it.each(params)('does not cache errors', async (...args) => {
     const fetch = cachedFetch(...args);
 
-    vi.spyOn(globalThis, 'fetch').mockImplementation(async () => new Response('', { status: 500 }));
+    jest
+      .spyOn(globalThis, 'fetch')
+      .mockImplementation(async () => new Response('', { status: 500 }));
     await fetch('https://example.com');
 
     expect(cache.put).not.toHaveBeenCalled();
@@ -53,7 +54,7 @@ describe(cachedFetch, () => {
 
   it.each(params)('returns cached response if any', async (...args) => {
     const cached = new Response();
-    vi.spyOn(cache, 'match').mockImplementation(async () => cached);
+    jest.spyOn(cache, 'match').mockImplementation(async () => cached);
     const fetch = cachedFetch(...args);
 
     const res = await fetch('https://example.com');
@@ -71,9 +72,11 @@ describe(cachedFetch, () => {
   });
 
   it.each(params)('sets Cache-Control header if none', async (...args) => {
-    vi.spyOn(globalThis, 'fetch').mockImplementation(
-      async () => new Response('', { headers: { 'Cache-Control': 'max-age: 1234' } }),
-    );
+    jest
+      .spyOn(globalThis, 'fetch')
+      .mockImplementation(
+        async () => new Response('', { headers: { 'Cache-Control': 'max-age: 1234' } }),
+      );
     const fetch = cachedFetch(...args);
 
     const res = await fetch('https://example.com');
@@ -86,35 +89,35 @@ describe(cachedFetch, () => {
   it.each(params)('caches each request with different headers', async (...args) => {
     const fetch = cachedFetch(...args);
 
-    vi.spyOn(globalThis, 'fetch').mockImplementation(async () => new Response('1'));
+    jest.spyOn(globalThis, 'fetch').mockImplementation(async () => new Response('1'));
     const first = await fetch('https://example.com', {
       headers: {
         'X-Foo': 'Bar',
       },
     });
-    vi.spyOn(globalThis, 'fetch').mockImplementation(async () => new Response('2'));
+    jest.spyOn(globalThis, 'fetch').mockImplementation(async () => new Response('2'));
     const second = await fetch('https://example.com', {
       headers: {
         'X-Foo': 'Baz',
       },
     });
 
-    const a = (cache.put as MockedFunction<(typeof Cache)['prototype']['put']>).mock
+    const a = (cache.put as jest.MockedFunction<(typeof Cache)['prototype']['put']>).mock
       .calls[0][0] as Request;
-    const b = (cache.put as MockedFunction<(typeof Cache)['prototype']['put']>).mock
+    const b = (cache.put as jest.MockedFunction<(typeof Cache)['prototype']['put']>).mock
       .calls[1][0] as Request;
     expect(a.headers).not.toEqual(b.headers);
     expect(await first.text()).not.toEqual(await second.text());
   });
 
   it.each(params)('fires fetch when cache missed', async (...args) => {
-    vi.spyOn(globalThis, 'fetch');
+    jest.spyOn(globalThis, 'fetch');
     const fetch = cachedFetch(...args);
 
     await fetch('https://example.com');
 
     expect(globalThis.fetch).toHaveBeenCalledWith(expect.any(Request));
-    const req = (globalThis.fetch as MockedFunction<typeof fetch>).mock.calls[0][0] as Request;
+    const req = (globalThis.fetch as jest.MockedFunction<typeof fetch>).mock.calls[0][0] as Request;
     expect(req.url).toBe('https://example.com/');
   });
 });
